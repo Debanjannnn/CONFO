810e81d77807d1a4ed4b47d3c1140d7be4b59185
Debanjan Mondal <debanjanmondalk2005@gmail.com>
2024-08-05 16:59:05 +0530

    first commit

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,12 @@
+// pages/index.tsx
+import React from 'react';// Adjust the path if necessary
+
+const Home = () => {
+  return (
+      <div>
+        This is the home page content.
+      </div>
+  );
+};
+
+export default Home;
---
+++ components/Sidebar.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { sidebarLinks } from '@/constant';
+import { cn } from '@/lib/utils';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link'; // Import Link from next/link
+import React from 'react';
+
+export const Sidebar = () => {
+  const pathname = usePathname();
+
+  return (
+    <section className='sticky left-0 top-0 flex h-screen flex-col w-fit justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[246px]'>
+      <div className='flex flex-1 flex-col gap-6'>
+        {sidebarLinks.map((link) => {
+          const isActive = pathname === link.route || pathname.startsWith(link.route);
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', {
+                'bg-blue-1': isActive,
+              })}
+            >
+              {link.label}
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
